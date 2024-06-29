65c404072aa34bf999d0ae7c7f08fe1552d1a909
fracta7 <fracta7@gmail.com>
2024-06-29 22:12:13 +0500

    updated navigation library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.fracta7.crafter"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
